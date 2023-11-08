a3f019fa867f03ddfe22933066809ff17d57b56a
Reginald Tan <redge.tan@gmail.com>
2023-11-08 16:35:53 -0500

    webpack config for single entrypoint and build only 1 file

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,21 @@ const webpack = require('webpack')
 
 const srcFiles = fs.readdirSync('./src')
 
-let entries = {
-  'gallery/gallery.min': './src/gallery.js',
-}
+// let entries = {
+//   'gallery/gallery.min': './src/gallery.js',
+// }
 
-// Compile vanta.xxxxx.js files
-for (let i = 0; i < srcFiles.length; i++) {
-  let file = srcFiles[i]
-  if (file.indexOf('vanta') == 0) {
-    let fileWithoutExtension = file.replace(/\.[^/.]+$/, "")
-    entries['dist/' + fileWithoutExtension + '.min'] = './src/' + file
-  }
+// // Compile vanta.xxxxx.js files
+// for (let i = 0; i < srcFiles.length; i++) {
+//   let file = srcFiles[i]
+//   if (file.indexOf('vanta') == 0) {
+//     let fileWithoutExtension = file.replace(/\.[^/.]+$/, "")
+//     entries['dist/' + fileWithoutExtension + '.min'] = './src/' + file
+//   }
+// }
+
+let entries = {
+  'dist/main.min': './src/entry.vanta.js'
 }
 
 module.exports = {
@@ -35,7 +39,7 @@ module.exports = {
     ],
   },
   optimization: {
-    minimize: true
+    minimize: true,
   },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
